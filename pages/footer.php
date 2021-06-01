c920f4f318a2fc81e586b8be4acd58bbc9f1857a
Harsha <maramsaiharsha128@gmail.com>
2021-06-02 01:25:45 +0530

    Initial commit

---
+++ pages/footer.php
@@ -0,0 +1,12 @@
+      <footer>
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-10">
+            <p>Protected by <strong><a href="https://maramsaiharsha.netlify.com" target="_blank">Harsha</a></strong></p>
+          </div>
+        </div>
+      </footer>
+    
+    </div>
+	
+  </body>
+</html>
